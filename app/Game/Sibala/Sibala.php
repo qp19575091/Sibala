8d73433bcc2cd180f1506b1b5b708e43e1195ba6
lejuallen <allenchen@leju.com.tw>
2024-11-17 16:45:54 +0800

    [feat] support tie category: all of a kind

--- app/Game/Sibala/Sibala.php
+++ app/Game/Sibala/Sibala.php
@@ -18,6 +18,10 @@ class Sibala
         $player1 = $this->groupByDice($this->player1);
         $player2 = $this->groupByDice($this->player2);
 
+        if (count($player1) === 1 && count($player2) === 1) {
+            return "Tie";
+        }
+
         if (count($player1) === 2 && count($player2) === 2) {
             if (array_key_last($player1) > array_key_last($player2)) {
                 return "Player1 win 100 with 3";
