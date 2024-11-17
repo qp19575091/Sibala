934c6360a507c5097fc96076d2ae59f725536dc5
lejuallen <allenchen@leju.com.tw>
2024-11-17 19:42:32 +0800

    [feat] rename ThreeOfAKind to OtherThreeOfAKind. =

--- app/Game/Sibala/Matcher/OtherThreeOfAKindMatcher.php
+++ app/Game/Sibala/Matcher/OtherThreeOfAKindMatcher.php
@@ -3,10 +3,10 @@
 namespace App\Game\Sibala\Matcher;
 
 use App\Game\Sibala\Category\Category;
-use App\Game\Sibala\Category\ThreeOfAKind;
+use App\Game\Sibala\Category\OtherThreeOfAKind;
 use App\Game\Sibala\Player;
 
-class ThreeOfAKindMatcher extends CategoryMatcher
+class OtherThreeOfAKindMatcher extends CategoryMatcher
 {
     public function isMatch(Player $player): bool
     {
@@ -15,6 +15,6 @@ class ThreeOfAKindMatcher extends CategoryMatcher
 
     public function getMatchCategory(Player $player): Category
     {
-        return new ThreeOfAKind();
+        return new OtherThreeOfAKind();
     }
 }
