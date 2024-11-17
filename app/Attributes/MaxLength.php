4abc42338792a85f61e07e83bd615d38d4f1ad19
lejuallen <allenchen@leju.com.tw>
2024-11-17 20:24:03 +0800

    [feat] add MinLength, MaxLength attribute

---
+++ app/Attributes/MaxLength.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Attributes;
+
+use Attribute;
+
+#[Attribute]
+class MaxLength extends ValidationRule
+{
+    public function __construct(
+        public readonly float $limit,
+        public readonly bool $strict = false
+    ) {
+    }
+
+    protected function pass($value): bool
+    {
+        if (is_array($value)) {
+            return $this->strict
+                ? count($value) < $this->limit
+                : count($value) <= $this->limit;
+        }
+
+        if (is_string($value)) {
+            return $this->strict
+                ? strlen($value) < $this->limit
+                : strlen($value) <= $this->limit;
+        }
+
+        return false;
+    }
+
+    protected function message(): string
+    {
+        return "Your value length can not over than $this->limit";
+    }
+}
+
