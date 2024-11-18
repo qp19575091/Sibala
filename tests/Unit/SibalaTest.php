ce514eb350b811c03c34780fa70159671560ea2c
qp19575091 <qp19575091@gmail.com>
2024-11-18 19:53:24 +0800

    [test] add test: test_win_with_same_category

---
+++ tests/Unit/SibalaTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SibalaTest extends TestCase
+{
+    public function test_win_with_same_category(): void
+    {
+        $this->assertEquals("Player2 win 100 with 3",
+            $this->game([1, 2, 1], [3, 2, 2], 100)
+        );
+    }
+
+    public function game($player1, $player2, $money): string
+    {
+        $game = new SiBala($player1, $player2, $money);
+        return $game->result();
+    }
+}
