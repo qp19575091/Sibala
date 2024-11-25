81f4b146f510e908b624a51a4983f093bf6cc421
qp19575091 <qp19575091@gmail.com>
2024-11-25 21:33:11 +0800

    [revert] [refactor] move winner logic to comparer because Player no need to compare

--- app/Game/Sibala/Comparer.php
+++ app/Game/Sibala/Comparer.php
@@ -5,32 +5,22 @@ namespace App\Game\Sibala;
 class Comparer
 {
     public function __construct(
-        private readonly Player $player1,
-        private readonly Player $player2
+        private readonly HandDice $handDice1,
+        private readonly HandDice $handDice2
     ) {
     }
 
     public function getResult(): int
     {
-        $handDice1 = $this->player1->getHandDice();
-        $handDice2 = $this->player2->getHandDice();
-
-        $category1 = $handDice1->getCategory();
-        $category2 = $handDice2->getCategory();
+        $category1 = $this->handDice1->getCategory();
+        $category2 = $this->handDice2->getCategory();
 
         $compareResult = $category1->type->value - $category2->type->value;
 
-        if ($compareResult === 0 && $handDice1->isNormalPoint()) {
-            return $handDice1->getSingePoint() - $handDice2->getSingePoint();
+        if ($compareResult === 0 && $this->handDice1->isNormalPoint()) {
+            return $this->handDice1->getSingePoint() - $this->handDice2->getSingePoint();
         }
 
         return $compareResult;
-    }
-
-    public function getWinner(): Player
-    {
-        return $this->getResult() > 0
-            ? $this->player1
-            : $this->player2;
     }
 }
--- app/Game/Sibala/SiBala.php
+++ app/Game/Sibala/SiBala.php
@@ -24,16 +24,23 @@ class SiBala
         $player1 = new Player($this->dice1, "Player1");
         $player2 = new Player($this->dice2, "Player2");
 
-        $comparer = new Comparer($player1, $player2);
-        if ($comparer->getResult() === 0) {
+        $handDice1 = $player1->getHandDice();
+        $handDice2 = $player2->getHandDice();
+
+        $category1 = $handDice1->getCategory();
+        $category2 = $handDice2->getCategory();
+
+        $comparer = new Comparer($handDice1, $handDice2);
+        $compareResult = $comparer->getResult();
+
+        if ($compareResult === 0) {
             return "Tie";
         }
 
-        $winner = $comparer->getWinner();
-        $category1 = $player1->getHandDice()->getCategory();
-        $category2 = $player2->getHandDice()->getCategory();
-
-        return $winner->name . " win " . $this->bet * $category1->multiplier * $category2->payoutRate . " with " .
-            $winner->getHandDice()->getSingePoint();
+        return $compareResult > 0
+            ? $player1->name . " win " . $this->bet * $category1->multiplier * $category2->payoutRate . " with " .
+            $handDice1->getSingePoint()
+            : $player2->name . " win " . $this->bet * $category2->multiplier * $category1->payoutRate . " with " .
+            $handDice2->getSingePoint();
     }
 }
